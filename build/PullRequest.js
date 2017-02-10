f6f0e0bc20ca574866ccb3d707768c32fa1b7b38
Kyle Mendes <kyleamendes@gmail.com>
2017-02-10 16:24:53 -0500

    Adding build

---
+++ build/PullRequest.js
@@ -0,0 +1,94 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _utils = require('./utils');
+
+var _consts = require('./consts');
+
+var request = require('request');
+
+function handleNew(payload, reply) {
+	// Get the issue, not the PR
+	request((0, _utils.constructGet)(payload.pull_request.issue_url), function (err, res, body) {
+		if (res.statusCode >= 200 && res.statusCode < 300) {
+			console.log('NEW Labels:', JSON.parse(body).labels);
+			var labels = JSON.parse(body).labels || [];
+
+			labels = labels.filter(function (label) {
+				return _consts.versionRegex.test(label.name);
+			});
+
+			var head = payload.pull_request.base.ref;
+			// If there aren't any version labels, and the PR isn't to a version branch,
+			// warn the developer to add labels, and label the PR "Incomplete"
+			if (!labels.length && !_consts.versionRegex.test(head) && head !== 'staging'
+			// head !== 'master'
+			) {
+					var feedback = '@' + payload.pull_request.user.login + ' - It looks like you forgot to label this PR with a version tag.  Please update your PR to include targetted version distrubtions.  Thanks!';
+					request((0, _utils.constructPost)(payload.pull_request.issue_url + '/comments', { body: feedback }));
+					request((0, _utils.constructPost)(payload.pull_request.issue_url + '/labels', ['incomplete']));
+
+					return reply('New PR -- Incomplete');
+				}
+
+			return reply('New PR -- Complete');
+		}
+
+		return null;
+	});
+}
+
+function handleMerge(payload, reply) {
+	// Get the issue, not the PR
+	request((0, _utils.constructGet)(payload.pull_request.issue_url), function (err, res, body) {
+		if (res.statusCode >= 200 && res.statusCode < 300) {
+			console.log('CLOSE Labels:', JSON.parse(body).labels);
+			var labels = JSON.parse(body).labels || [];
+
+			labels = labels.filter(function (label) {
+				return _consts.versionRegex.test(label.name);
+			});
+
+			// If there were version labels, create new PRs targetting those branches
+			if (labels.length) {
+				(function () {
+					var head = payload.pull_request.head.ref;
+					labels.forEach(function (label) {
+						var pr = {
+							title: label.name + ' -- ' + payload.pull_request.title,
+							body: '# Merging from branch "' + head + '" to version ' + label.name + '\n\n' + payload.pull_request.body,
+							base: label.name,
+							head: head
+						};
+
+						console.log('PR', pr);
+						request((0, _utils.constructPost)(payload.repository.url + '/pulls', pr));
+					});
+				})();
+			}
+
+			return reply('Closed and created');
+		}
+
+		return reply('Closed');
+	});
+}
+
+function PullRequest(req, reply) {
+	var payload = req.payload;
+
+	if (payload.action === 'opened') {
+		return handleNew(payload, reply);
+	}
+
+	if (payload.action === 'closed' && payload.pull_request.merged_at) {
+		return handleMerge(payload, reply);
+	}
+
+	return reply('Got a pull request!!!');
+}
+
+exports.default = PullRequest;
---
+++ build/server.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var _PullRequest = require('./PullRequest');
+
+var _PullRequest2 = _interopRequireDefault(_PullRequest);
+
+var _Review = require('./Review');
+
+var _Review2 = _interopRequireDefault(_Review);
+
+function _interopRequireDefault(obj) {
+	return obj && obj.__esModule ? obj : { default: obj };
+}
+
+var Hapi = require('hapi');
+
+// Create a server with a host and port
+var server = new Hapi.Server();
+server.connection({
+	host: 'localhost',
+	port: 1312
+});
+
+function handleGithubEvent(req, reply) {
+	var event = req.headers['x-github-event'];
+
+	if (event === 'pull_request') {
+		console.log('got a PR');
+		return (0, _PullRequest2.default)(req, reply);
+	}
+
+	if (event === 'pull_request_review') {
+		console.log('got a review');
+		return (0, _Review2.default)(req, reply);
+	}
+
+	return reply();
+}
+
+server.route({
+	method: '*',
+	path: '/{path*}',
+	handler: function handler(request, reply) {
+		if (request.headers['x-github-event']) {
+			return handleGithubEvent(request, reply);
+		}
+
+		console.log('Got it', request.params, request.method);
+		return reply('Thanks');
+	}
+});
+
+// Start the server
+server.start(function (err) {
+
+	if (err) {
+		throw err;
+	}
+	console.log('Server running at:', server.info.uri);
+});
