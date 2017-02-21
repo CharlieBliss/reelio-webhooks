021880727875ced6be2524056fec88feeb95ab17
Kyle Mendes <kyleamendes@gmail.com>
2017-02-21 10:36:20 -0500

    Adding support for new ticket prepending

--- app/consts/api.js
+++ app/consts/api.js
@@ -1,3 +1,3 @@
 export const token = '4d22f6a942691241c5d60d4b4d7b266bb66e94c8'
 export const versionRegex = /\d+[\.\d]+/
-export const jiraRegex = /(ON-\d+)/gi
+export const jiraRegex = /((ON|FRONT)-\d+)/gi
