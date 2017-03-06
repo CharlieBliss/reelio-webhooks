9ceec1443c4a4b0f040dad73bd53a6fed071c05e
Kyle Mendes <kyleamendes@gmail.com>
2017-03-06 11:57:09 -0500

    Fixing build errors

--- app/consts/api.js
+++ app/consts/api.js
@@ -1,4 +1,4 @@
 export const GITHUB_TOKEN = '38f162f775da66e500124282520dce7777a255be'
 export const JIRA_TOKEN = 'ZGV2b3BzQHJlZWxpb2xhYnMuY29tOkdhYWVDX0RCZWJwNkV0NGEtd3dkNmZKLTZkbktRWjhw'
-export const versionRegex = /\d+[\.\d]+/
+export const versionRegex = /\d+[.\d]+/
 export const jiraRegex = /((TEST|ON|FRONT)-\d+)/gi
