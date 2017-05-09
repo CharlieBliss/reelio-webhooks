11239d635656d32bc9b72c1867c4ef3a1db8e55d
dillonmcroberts <dillon.mcroberts@gmail.com>
2017-05-08 22:14:00 -0400

    test for ticketless && featureless PR

--- test/payloads/github/pullRequest.js
+++ test/payloads/github/pullRequest.js
@@ -325,6 +325,89 @@ export const pullRequestFeatureless = {
   },
 }
 
+export const ticketAndFeatureless = {
+  "action": "opened",
+  "pull_request": {
+    "url": "https://api.github.com/repos/Kyle-Mendes/public-repo/pulls/1",
+    "id": 34778301,
+    "html_url": "https://github.com/Kyle-Mendes/public-repo/pull/1",
+		"issue_url": "https://api.github.com/repos/Kyle-Mendes/public-repo/issues/1",
+    "state": "open",
+    "title": "Update the README with new information",
+    "user": {
+      "login": "Kyle-Mendes",
+      "id": 6752317,
+      "url": "https://api.github.com/users/Kyle-Mendes",
+      "html_url": "https://github.com/Kyle-Mendes",
+    },
+    "body": "This is a pretty simple change that we need to pull into staging",
+    "head": {
+      "label": "Kyle-Mendes:changes",
+      "ref": "changes",
+      "sha": "0d1a26e67d8f5eaf1f6ba5c57fc3c7d91ac0fd1c",
+      "user": {
+        "login": "Kyle-Mendes",
+        "id": 6752317,
+        "url": "https://api.github.com/users/Kyle-Mendes",
+        "html_url": "https://github.com/Kyle-Mendes",
+      },
+      "repo": {
+        "id": 35129377,
+        "name": "public-repo",
+        "full_name": "Kyle-Mendes/public-repo",
+        "owner": {
+          "login": "Kyle-Mendes",
+          "id": 6752317,
+          "url": "https://api.github.com/users/Kyle-Mendes",
+          "html_url": "https://github.com/Kyle-Mendes",
+        },
+        "html_url": "https://github.com/Kyle-Mendes/public-repo",
+        "description": "",
+        "url": "https://api.github.com/repos/Kyle-Mendes/public-repo",
+        "default_branch": "staging"
+      }
+    },
+    "base": {
+      "label": "Kyle-Mendes:staging",
+      "ref": "staging",
+      "sha": "9049f1265b7d61be4a8904a9a27120d2064dab3b",
+      "user": {
+        "login": "Kyle-Mendes",
+        "id": 6752317,
+        "url": "https://api.github.com/users/Kyle-Mendes",
+        "html_url": "https://github.com/Kyle-Mendes",
+      },
+      "repo": {
+        "id": 35129377,
+        "name": "public-repo",
+        "full_name": "Kyle-Mendes/public-repo",
+        "owner": {
+          "login": "Kyle-Mendes",
+          "id": 6752317,
+          "url": "https://api.github.com/users/Kyle-Mendes",
+          "html_url": "https://github.com/Kyle-Mendes",
+        },
+        "html_url": "https://github.com/Kyle-Mendes/public-repo",
+        "url": "https://api.github.com/repos/Kyle-Mendes/public-repo",
+        "default_branch": "staging"
+      }
+    },
+  },
+  "repository": {
+    "id": 35129377,
+    "name": "public-repo",
+    "full_name": "Kyle-Mendes/public-repo",
+    "owner": {
+      "login": "Kyle-Mendes",
+      "id": 6752317,
+      "url": "https://api.github.com/users/Kyle-Mendes",
+      "html_url": "https://github.com/Kyle-Mendes",
+    },
+    "html_url": "https://github.com/Kyle-Mendes/public-repo",
+    "url": "https://api.github.com/repos/Kyle-Mendes/public-repo",
+  },
+}
+
 export const pullRequestMultiTickets = {
   "action": "opened",
   "number": 1,
