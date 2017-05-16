c6325a6208b556adb6502ff819c1f9fd28e15475
Kyle Mendes <kyleamendes@gmail.com>
2017-05-16 11:27:13 -0400

    Deprecating support for 'ON' tickets

--- src/consts/api.js
+++ src/consts/api.js
@@ -1,3 +1,3 @@
 export const GITHUB_TOKEN = '38f162f775da66e500124282520dce7777a255be'
 export const JIRA_TOKEN = 'ZGV2b3BzQHJlZWxpb2xhYnMuY29tOkdhYWVDX0RCZWJwNkV0NGEtd3dkNmZKLTZkbktRWjhw'
-export const jiraRegex = /((XYZ|ON|FRONT|BACK|RA|FAT)-\d+)/gi
+export const jiraRegex = /((XYZ|FRONT|BACK|RA|FAT)-\d+)/gi
