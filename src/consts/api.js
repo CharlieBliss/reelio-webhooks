a58bd06b524af235b796e3d00cf3e3dfa5b00bbb
Kyle Mendes <kyleamendes@gmail.com>
2017-09-07 14:55:47 -0400

    Adding devops to ticket list

--- src/consts/api.js
+++ src/consts/api.js
@@ -1,6 +1,6 @@
 export const GITHUB_TOKEN = process.env.github_token || '38f162f775da66e500124282520dce7777a255be'
 export const JIRA_TOKEN = 'ZGV2b3BzQHJlZWxpb2xhYnMuY29tOkdhYWVDX0RCZWJwNkV0NGEtd3dkNmZKLTZkbktRWjhw'
 export const TICKET_BASE = 'https://reelio.atlassian.net/rest/api/2/issue'
-export const jiraRegex = /((XYZ|FRONT|BACK|RA|FAT)-\d+)/gi
-export const jiraRegexWithDescription = /((XYZ|FRONT|BACK|RA|FAT)-\d+)(.*)/gi
-export const deployRegex = /((XYZ|FRONT|BACK|RA|FAT)-\d+)\s::\s(.+?(?=]))/
+export const jiraRegex = /((XYZ|FRONT|BACK|RA|FAT|DEVOPS)-\d+)/gi
+export const jiraRegexWithDescription = /((XYZ|FRONT|BACK|RA|FAT|DEVOPS)-\d+)(.*)/gi
+export const deployRegex = /((XYZ|FRONT|BACK|RA|FAT|DEVOPS)-\d+)\s::\s(.+?(?=]))/
