306f595de8258dae6799a544608a0f02edc235ac
charliebliss <charles.m.bliss@gmail.com>
2018-05-02 11:34:45 -0400

    Update credentials again to reelioman

--- src/handlers/github/index.js
+++ src/handlers/github/index.js
@@ -29,7 +29,7 @@ export function handle(event, context, callback) {
 	console.log('---------------------------------');
 	console.log(`Github-Event: "${githubEvent}" with action: "${action}"`);
 	console.log('---------------------------------');
-	console.log('config: ', org, repo, handler, config)
+	console.log('config: ', org, repo, handler)
 	/* eslint-enable */
 
 	firebase.log('github', fullRepo, githubEvent, action, payload)
